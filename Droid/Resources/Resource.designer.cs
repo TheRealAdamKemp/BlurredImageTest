fbdad0695e2d9184313a30ed6db50d746bb272da
Adam Kemp <adam@adamkemp.com>
2015-05-13 22:34:26 -0500

    Updated packages.

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -26,6 +26,8 @@ namespace BlurredImageTest.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::BlurredImageTest.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::BlurredImageTest.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -56,6 +58,25 @@ namespace BlurredImageTest.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
